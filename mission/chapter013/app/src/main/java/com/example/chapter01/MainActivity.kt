576061532819000c9809e932339be6ffc1deee59
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2024-09-27 03:31:29 +0900

    mission : 1주차 미션 완성

---
+++ mission/chapter013/app/src/main/java/com/example/chapter01/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.chapter01
+
+import com.example.chapter01.R
+import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main)
+
+
+    }
+
+}
