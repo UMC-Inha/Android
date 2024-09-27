cdca7154a9b404275deb3392e0cd4a36774392f7
한만욱 <manwook-han@hanman-ug-ui-MacBookPro.local>
2024-09-27 18:01:07 +0900

    feat : 커스텀뷰 만들기

--- mission/chapter013/app/src/main/java/com/example/chapter01/MainActivity.kt
+++ mission/chapter013/app/src/main/java/com/example/chapter01/MainActivity.kt
@@ -10,8 +10,13 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main)
-
-
     }
-
+    private fun initEmotionView(){
+        val emotionSrc=R.drawable.emotion1
+        val emotion_text= listOf("더없이 행복한 하루였어요","들뜨고 흥분되요","평범한 하루였어요","생각이 많아지고 불안해요","부글부글 화가 나요")
+        val emotion_src= listOf(emotionSrc,emotionSrc,emotionSrc,emotionSrc,emotionSrc)
+    }
 }
+
+// 애뮬 -> 젤 작은것 기준으로 할것
+//일단 다 넣어 보고 string 넣어오기
---
+++ mission/chapter013/app/src/main/java/com/example/chapter01/shared/EmotionView.kt
@@ -0,0 +1,47 @@
+package com.example.chapter01.shared
+
+import android.content.Context
+import android.view.View.inflate
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.compose.ui.unit.Constraints
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.example.chapter01.R
+
+//    1. Application 환경에 대한 정보 제공
+//  Context는 애플리케이션 환경에 대한 정보를 제공합니다. 이를 통해 애플리케이션의 리소스, 데이터베이스, 파일 시스템 접근 등과 같은 기능에 접근할 수 있습니다.
+//
+//2. UI 요소에 대한 참조 제공
+//  Context는 UI 구성 요소와 상호작용할 때 필요합니다. 예를 들어, TextView와 같은 뷰를 생성하고 스타일을 적용하는 데 사용됩니다.
+//
+//3. 리소스 접근
+//  Context를 사용하여 애플리케이션의 리소스에 접근할 수 있습니다. 예를 들어, 문자열, 이미지, 레이아웃 파일 등을 가져오는 데 사용됩니다.
+
+// 컨텐스트는 레이아웃이나 정보등을 담고 있는 것
+class EmotionView constructor(
+    context:Context,
+    text:String="기본 텍스트",
+    svgPath:Int
+): ConstraintLayout(context){
+    private var emotionText: String = text
+    private var emotionSvgPath: Int = svgPath
+    private val textView: TextView
+    private val imageView: ImageView
+    init {
+//        xml 레이아웃을 뷰 객체로 변환
+        inflate(context, R.layout.mood_item, this)
+         textView = findViewById(R.id.mood_text)
+         imageView = findViewById(R.id.mood_icon)
+        initView()
+    }
+    private fun initView(){
+        setEmotionText(emotionText)
+        setEmotionImage(emotionSvgPath)
+    }
+    private fun setEmotionText(text: String) {
+        textView.text = text
+    }
+    private fun setEmotionImage(resourceId: Int) {
+        imageView.setImageResource(resourceId)
+    }
+}
